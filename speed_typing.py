2147c5ce14e1c10a1d1288e7744b48d7db170705
Prithvi Rohira <74297252+prithvirohira8@users.noreply.github.com>
2021-05-12 18:09:39 +0530

    Update speed_typing.py

--- speed_typing.py
+++ speed_typing.py
@@ -3,56 +3,48 @@ import random
 
 print("Python Speed Typing Test")
 print("\n\n")
-lst_of_paragraphs = [
-    "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma, which is living with the results of other people's thinking",
-    "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
-    "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
-    "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
-    "Do not go where the path may lead, go instead where there is no path and leave a trail"]
-s = 'yes'
+lst_of_paragraphs = ["Python is a fantastic programming language","Java is an object oriented programming language","C++ has the fastest execution time for any program","Python is the most popular language"] 
 if s == input("Press type yes when u are ready: "):
     print("\n(Please enter the words with the correct punctuation marks too)\n\n")
     test_string = random.choice(lst_of_paragraphs)
     print(test_string)
-    print("\n")
+    print("\n\n\n")
     t1 = time.time()
-    input_string = input("Type the lines here: ")
+    input_string = input("Type the lines here: ") 
     t2 = time.time()
     time_taken = (t2 - t1)
+
+
 
 res1 = test_string.split()
 res2 = input_string.split()
 
-test_lst_lst = []
-input_lst_lst = []
+test_lst_set = set()
+input_lst_set = set()
 
 for i in res1:
-    test_lst_lst.append(i)
-
+    test_lst_set.add(i)
+    
 for i in res2:
-    input_lst_lst.append(i)
-
-sorted(test_lst_lst)
-sorted(input_lst_lst)
+    input_lst_set.add(i)
 
 count = 0
-for i in input_lst_lst:
-    for j in test_lst_lst:
-        if i == j:
-            count += 1
-            test_lst_lst.remove(i)
-            break;
+for i in input_lst_set:
+    if(i in test_lst_set):
+        count += 1
+        test_lst_set.discard(i)
 
-time_taken_in_mins = time_taken / 60
-typing_speed = int(count // time_taken_in_mins)
-typing_accuracy = (count / len(res1)) * 100
+time_taken_in_mins = time_taken/60
+typing_speed = int(count//time_taken_in_mins)
+
+typing_accuracy = (count/len(res1))*100
 print("The no of words typed correctly are: {}".format(count))
 
-if (len(test_lst_set) != 0):
-    print("\nThe incorrectly typed/missing words are: ", )
+if(len(test_lst_set) != 0):
+    print("\nThe incorrectly typed/missing words are: ",)
     for i in test_lst_set:
-        print("{} ".format(i), end='')
-
+        print("{} ".format(i),end='')
+        
 print("\n\nYour typing speed is {} words per minute".format(typing_speed))
 print("\nYour typing accuracy is {:.2f}% ".format(typing_accuracy))
 if typing_speed<35:
